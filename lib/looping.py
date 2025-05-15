f4436bb9b1867341d99ef126353924471de6c172
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-15 09:58:25 +0300

    learnt about loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):  # Countdown from 10 to 1
+        print(i)
+    print("Happy New Year!")  # Print the final message
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []  # Initialize an empty list to store squared integers
+    for i in int_list:
+        if isinstance(i, int):
+            squared_list.append(i ** 2)  # Append the squared value to the list
+    return squared_list  # Return the list of squared integers
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
